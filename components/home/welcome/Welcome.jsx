3f0e6883657d119791982c140fc22250aad08af7
calebmutisya <mutisyacalebmusau@gmail.com>
2025-01-07 12:31:59 +0300

    Welcome page done

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,12 +1,60 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, TextInput, TouchableOpacity, Image, FlatList } from 'react-native'
+
+import { useRouter } from 'expo-router'
 
 import styles from './welcome.style'
+import { icons, SIZES } from '../../../constants'
+
+const jobTypes = ["Full-time", "Part-time", "Contractor"]
 
 const Welcome = () => {
+
+  const router = useRouter()
+  const [activeJobType, setActiveJobType] = useState('Full-time')
+
   return (
     <View>
-      <Text>Welcome</Text>
+      <View styles={styles.container}>
+        <Text style={styles.userName}>Hello Caleb</Text>
+        <Text style={styles.welcomeMessage}>Find your perfect job</Text>
+      </View>
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput 
+            style={styles.searchInput}
+            value=""
+            onChange={() => {}}
+            placeholder="What are you looking for?"
+          />
+        </View>
+        <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+          <Image 
+            source={icons.search}
+            resizeMode="contain"
+            style={styles.searchBtnImage}
+          />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.tabsContainer}>
+        <FlatList data={jobTypes} 
+        renderItem={({item})=>(
+          <TouchableOpacity
+           style={styles.tab(activeJobType, item )}
+           onPress={()=>{
+            setActiveJobType(item);
+            router.push(`/serach/${item}`)
+           }}
+          >
+            <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
+          </TouchableOpacity>
+        )}
+        keyExtractor={item => item}
+        contentContainerStyle ={{columnGap: SIZES.small}}
+        horizontal // Appear next to each other
+        showsHorizontalScrollIndicator= {false}
+        />
+      </View>
     </View>
   )
 }
