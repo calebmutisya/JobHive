15fcc8a2c691d9b8d33d605c30a85e2f46320093
calebmutisya <mutisyacalebmusau@gmail.com>
2025-01-10 15:12:19 +0300

    Added search functionality

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -8,7 +8,7 @@ import { icons, SIZES } from '../../../constants'
 
 const jobTypes = ["Full-time", "Part-time", "Contractor"]
 
-const Welcome = () => {
+const Welcome = ({searchTerm, setSearchTerm, handleClick}) => {
 
   const router = useRouter()
   const [activeJobType, setActiveJobType] = useState('Full-time')
@@ -23,12 +23,12 @@ const Welcome = () => {
         <View style={styles.searchWrapper}>
           <TextInput 
             style={styles.searchInput}
-            value=""
-            onChange={() => {}}
+            value={searchTerm}
+            onChange={(text) => setSearchTerm(text)}
             placeholder="What are you looking for?"
           />
         </View>
-        <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+        <TouchableOpacity style={styles.searchBtn} onPress={handleClick}>
           <Image 
             source={icons.search}
             resizeMode="contain"
@@ -43,7 +43,7 @@ const Welcome = () => {
            style={styles.tab(activeJobType, item )}
            onPress={()=>{
             setActiveJobType(item);
-            router.push(`/serach/${item}`)
+            router.push(`/search/${item}`)
            }}
           >
             <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
